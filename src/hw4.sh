f959f033eb72b79510149fbc13f619bd7c00b662
sharonwx54 <123585394+sharonwx54@users.noreply.github.com>
2023-01-28 12:48:06 -0500

    Delete src directory

--- src/hw4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Sample script file to run your code. Feel free to change it.
-# Run this script using ./hw4.sh train_file test_file
-# Example:  ./hw4.sh ../data/covtype.scale.trn.libsvm ../data/covtype.scale.tst.libsvm
-
-echo "Running using train file at" $1 "and test file at" $2
-python main.py $1 $2
-#echo "Running CovType using train file at" $1 "and test file at" $2
-#python main.py ../data/covtype.scale.trn.libsvm ../data/covtype.scale.tst.libsvm
